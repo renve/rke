c031517ded850feaf97a05e9b164b22c15e34658
renve <jventi49@gmail.com>
2020-01-01 13:51:43 -0500

    added unzip to script

--- backup/dev-setup.sh
+++ backup/dev-setup.sh
@@ -17,6 +17,8 @@ wget https://github.com/rancher/rke/releases/download/v0.3.2/rke_linux-amd64
 chmod 755 rke*
 mv rke* /usr/local/bin/rke
 
+sudo apt-get install unzip -y
+
 # install helm3
 https://get.helm.sh/helm-v3.0.2-linux-amd64.tar.gz
 tar -zxf helm*
