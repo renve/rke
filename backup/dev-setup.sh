9b30a3f7a77e8c258419a3a34f9266dc50110321
renve <jventi49@gmail.com>
2020-01-01 14:10:30 -0500

    mod on script

--- backup/dev-setup.sh
+++ backup/dev-setup.sh
@@ -15,14 +15,14 @@ sudo apt-get install docker.io -y
 wget https://github.com/rancher/rke/releases/download/v0.3.2/rke_linux-amd64
 
 chmod 755 rke*
-mv rke* /usr/local/bin/rke
+sudo mv rke_linux-amd64 /usr/local/bin/rke
 
 sudo apt-get install unzip -y
 
 # install helm3
-https://get.helm.sh/helm-v3.0.2-linux-amd64.tar.gz
+wget https://get.helm.sh/helm-v3.0.2-linux-amd64.tar.gz
 tar -zxf helm*
 
-mv linux-amd64/helm /usr/local/bin/helm
+sudo mv linux-amd64/helm /usr/local/bin/helm
 
 
